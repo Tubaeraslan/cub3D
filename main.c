542601dc6a5bf6c8236ed94c91edae681acb8f38
Tuba Eraslan <teraslan@k1m38s05.42istanbul.com.tr>
2025-08-22 17:49:47 +0300

    player_position fonksiyonu eklendi. semihe sor olmazsa parse dan ekleriz

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 /*   By: teraslan <teraslan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 11:55:12 by skaynar           #+#    #+#             */
-/*   Updated: 2025/08/22 16:01:57 by teraslan         ###   ########.fr       */
+/*   Updated: 2025/08/22 17:45:18 by teraslan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void set_player_position_from_map(t_data *data)
+{
+    for (int y = 0; data->char_map[y]; y++) {
+        for (int x = 0; data->char_map[y][x]; x++) {
+            if (data->char_map[y][x] == 'N' || data->char_map[y][x] == 'S' ||
+                data->char_map[y][x] == 'E' || data->char_map[y][x] == 'W') {
+                data->player->posX = x + 0.5;
+                data->player->posY = y + 0.5;
+                data->player->start_pos = data->char_map[y][x];
+                data->char_map[y][x] = '0';
+                return;
+            }
+        }
+    }
+}
 
 int main(int ac, char **av)
 {
@@ -33,8 +49,7 @@ int main(int ac, char **av)
 	//fplayer_position(data->char_map,data);
 	// data->player->mapX=3;
 	// data->player->mapY=3;
-	data->player->posX = 3.5;
-	data->player->posY = 3.5;
+	set_player_position_from_map(data);
 	data->text_width = 0;
 	data->text_height = 0;
     execute(data);
